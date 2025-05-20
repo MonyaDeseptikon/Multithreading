b8713723ab0a22333130dc03b061ed34ebd1a8fa
Monya <isadom@mail.ru>
2025-05-20 21:17:53 +0400

    Initial commit

---
+++ src/main/java/Lock.java
@@ -0,0 +1,55 @@
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Program {
+    static Integer res = Integer.valueOf(0);
+
+    public static void main(String[] args) {
+
+        ReentrantLock locker = new ReentrantLock(); // создаем заглушку
+        for (int i = 0; i < 5; i++) {
+
+            Thread t = new Thread(new CommonResource(locker));
+            t.start();
+        }
+    }
+
+
+    static class CommonResource implements Runnable {
+
+        ReentrantLock locker;
+
+        CommonResource(ReentrantLock lock) {
+            this.locker = lock;
+        }
+
+        public void run() {
+
+
+            try {
+                locker.lock(); // устанавливаем блокировку
+                for (int i = 1; i < 5; i++) {
+
+                    System.out.printf("%s %d \n", Thread.currentThread().getName(), res);
+                    res++;
+                    Thread.sleep(100);
+
+                }
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            } finally {
+                locker.unlock(); // снимаем блокировку
+            }
+        }
+    }
+
+
+}
+
+
+
+
+
+
+
+
